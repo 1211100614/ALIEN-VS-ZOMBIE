3ffd13ab42fe7ec533ef36dcf6e703aa5141236d
1211100614 <124138330+1211100614@users.noreply.github.com>
2023-02-01 20:27:34 +0800

    Add files via upload

---
+++ ASSIGNMENT/PART1.md
@@ -0,0 +1,60 @@
+# Part 1
+
+## Video Demo
+
+Please provide the YouTube link to your [Video Demo](https://youtu.be/PVEK3W9OE-w).
+
+## Minimum Requirements
+
+### Completed
+
+List all the features completed.
+1. main menu
+2. game board
+3. command 
+4. users can insert their own preference for the gameboard
+
+### To Do
+
+List all the features not yet done. Remove this section if there is no incomplete requirements.
+
+1. quit the game 
+2. zombie functions
+3. placing the alien and zombie
+
+## Additional Features
+
+Describe the additional features that has been implemented.
+
+## Contributions
+
+List down the contribution of each group members.
+
+For example:
+
+### HEMMA 
+
+1. Randomly generate game board.
+2. generate game display menu.
+
+### TIVAASHENY
+
+1. Generate game displaymenu. 
+2. Randomly generate game board.
+ 
+
+### YUVAN
+
+1. Generate game displaymenu.
+2. Randomly generate gameboard.
+
+
+## Problems Encountered & Solutions
+
+Describe the problems encountered and provide the solutions / plan for the solutions.
+
+Problems
+1. We couldn't create a grid-like layout for the game board properly in order.
+
+Solutions 
+1. We refer to the lecture and lab notes.
---
+++ ASSIGNMENT/main.cpp
@@ -0,0 +1,212 @@
+// *********************************************************
+// Course: TCP1101 PROGRAMMING FUNDAMENTALS
+// Year: Trimester 1, 2022/23 (T2215)
+// Lab: T11L
+// Names: TIVAASHENY A/P ANANTHAN | HEMMA RAVINDRAN | YUVAN A/L GANESAN
+// IDs: 1211100614 | 1211102066 | 1201103498
+// Emails: 1211100614@student.mmu.edu.my | 1211102066@student.mmu.edu.my | 1201103498@student.mmu.edu.my
+// Phones: 0196900596 | 0137008516 | 0143859251
+// *********************************************************
+
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+#include <iomanip>
+using namespace std;
+
+class Board
+{
+private:
+    vector<vector<char>> board_size;
+    int column, row , zombie;
+
+public:
+    Board(int board_Columns = 5, int board_Rows = 9, int zombie_count = 1);
+    void init(int board_Columns, int board_Rows, int zombie_count);
+    void display() const;
+};
+
+Board::Board(int board_Columns, int board_Rows, int zombie_count)
+{
+    cout << endl;
+    cout << "Board Settings" << endl;
+    cout << "----------------" << endl;
+
+    cout << "Enter rows => ";
+    cin >> board_Rows;
+    cout << "Enter columns => ";
+    cin >> board_Columns;
+    cout << "Zombie count => ";
+    cin >> zombie_count;
+
+    init(board_Columns, board_Rows, zombie_count);
+}
+
+void Board::init(int board_Columns, int board_Rows, int zombie_count)
+{
+    row= board_Rows;
+    column= board_Columns;
+    zombie = zombie_count;
+
+    
+
+    board_size.resize(row);
+    for (int i = 0; i < row; ++i)
+    {
+        board_size[i].resize(column);
+    }
+
+
+}
+
+void Board::display() const
+{
+
+    cout << setw(15);
+    cout << " .: Alien vs Zombie :." << endl;
+
+    for (int i = 0; i < row; ++i)
+    {
+        cout << "   ";
+        for (int j = 0; j < column; ++j)
+        {
+            cout << "+-";
+        }
+        cout << "+" << endl;
+
+        cout << setw(2) << (row - i) << " ";
+
+        for (int j = 0; j < column; ++j)
+        {
+            cout << "|" << board_size[i][j];
+        }
+        cout << "|" << endl;
+    }
+
+    cout << "   ";
+    for (int j = 0; j < column; ++j)
+    {
+        cout << "+-";
+    }
+    cout << "+" << endl;
+
+    cout << "   ";
+    for (int j = 0; j < column; ++j)
+    {
+        int digit = (j + 1) / 10;
+        cout << " ";
+        if (digit == 0)
+            cout << " ";
+        else
+            cout << digit;
+    }
+    cout << endl;
+    cout << "   ";
+    for (int j = 0; j < column; ++j)
+    {
+        cout << " " << (j + 1) % 10;
+    }
+    cout << endl
+         << endl;
+}
+
+
+
+void gameBoardDisplay()
+{
+    Board board;
+    board.display();
+  
+}
+
+void displayMenu()
+{
+
+    cout << "+----------------------------------------------------------+" << endl;
+    cout << "|                        ALIEN VS ZOMBIE                   |" << endl;
+    cout << "|==========================================================|" << endl;
+    cout << "| Select:                                                  |" << endl;
+    cout << "| 1 => Display default game settings                       |" << endl;
+    cout << "| 2 => Display Help                                        |" << endl;
+    cout << "|----------------------------------------------------------|" << endl;
+    cout << "| 3 => Change default game settings and START the game.    |" << endl;
+    cout << "| 4 => Save game                                           |" << endl;
+    cout << "| 5 => Load a game                                         |" << endl;
+    cout << "|----------------------------------------------------------|" << endl;
+    cout << "| Q => Quit                                                |" << endl;
+    cout << "+----------------------------------------------------------+" << endl;
+    cout << endl;
+    
+}
+
+
+
+void displayData(int rows, int columns, int zombies)
+{
+    cout << "BOARD ROWS = " << rows << endl;
+    cout << "BOARD COLUMNS = " << columns << endl;
+    cout << "ZOMBIE COUNT = " << zombies << endl;
+    cout << endl;
+}
+
+
+void displayHelp()
+{
+    cout << "1. up = Move up"  << endl;
+    cout << "2. down = Move down" << endl;
+    cout << "3. left = Move left"  << endl;
+    cout << "4. right = Move right" << endl;
+    cout << "5. arrow = Change the direction of an arrow" << endl;
+    cout << "6. help = Display these user commands" << endl;
+    cout << "7. save = Save the game" << endl;
+    cout << "8. load = Load the game" << endl;
+    cout << "9. quit = Quit the game" << endl;
+    cout << endl;
+}
+
+int main()
+{
+    int rows = 5;
+    int columns = 9;
+    int zombies = 1;
+
+
+    char choice;
+    bool done = false;
+    displayMenu();
+    do
+    {
+        cout << "Choice =>";
+        cin >> choice;
+        choice = toupper(choice);
+        cout << endl;
+        switch (choice)
+        {
+        case '1':
+            displayData(rows,columns,zombies);
+            break;
+        case '2':
+            displayHelp();
+            break;
+        case '3':
+            gameBoardDisplay();
+            break;
+        case '4':
+            ;
+            break;
+        case '5':
+            ;
+            break;
+        case 'Q':
+            done = true;
+            break;
+        default:
+            cout << "Invalid selection, try again!" << endl;
+            cout << endl;
+            break;
+        }
+
+    } while (!done);
+    cout << endl;
+    cout << "THANKS FOR PLAYING!" << endl;
+}
